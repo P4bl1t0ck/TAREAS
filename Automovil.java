326a4c2307988f9e5ebf6733b477765010a085ae
P4bl1t0ck <130107066+P4bl1t0ck@users.noreply.github.com>
2023-10-06 10:11:50 -0500

    Add files via upload

---
+++ Automovil.java
@@ -0,0 +1,12 @@
+// clase creada
+public class Automovil {
+    // orpiedades y metodos
+    String marca="Lamborguini";
+    Integer modelo;
+    Integer motor;
+    Integer numerodepuertas;
+    Integer cantidadasientos;
+    Integer velocidadmaxima;
+
+
+}
---
+++ PrincipalAutomovil.java
@@ -0,0 +1,13 @@
+// Main
+public class PrincipalAutomovil {
+    public static void main(String[] args) {
+        ///
+        Automovil lamborguini = new Automovil();
+        Automovil toyota = new Automovil();
+        Automovil suzuki = new Automovil();
+
+        lamborguini.marca = "Toyota";
+        toyota.marca = "";
+
+    }
+}
