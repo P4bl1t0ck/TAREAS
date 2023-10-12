c677869703f5e1bbdded558245efcd413c920378
P4bl1t0ck <130107066+P4bl1t0ck@users.noreply.github.com>
2023-10-12 11:13:09 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        String var="Hola Mundo";
+        Integer num=23333;
+        System.out.println("El Ecuador les dice : " + var + num);
+
+    }
+}
---
+++ HolaPablo.java
@@ -0,0 +1,12 @@
+public class HolaPablo {
+    public static void main(String[] args) {
+
+        String var="Hola Pablo";
+	String vari="Estas Hackeado";
+        Integer num=1;
+        System.out.println("El Ecuador les dice hola a : " + var + num+vari);
+
+    }
+}
+
+
